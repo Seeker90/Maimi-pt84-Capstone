66623a428fdb7eb4005df9afdd4dc904a22df1a4
Seeker90 <chris.carver90@icloud.com>
2025-12-05 02:20:39 +0000

    test2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: cd src && gunicorn wsgi:app --bind 0.0.0.0:$PORT
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import {
-    defineConfig
-} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000
-    },
-    build: {
-        outDir: 'dist'
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      }
     }
+  }
 })
